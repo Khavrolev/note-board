09aeb457407c59877d94b9d7e5b4a2fcb28da09a
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-03-21 16:17:54 +0300

    delete env files

--- note-board-frontend/src/contexts/SocketProvider.js
+++ note-board-frontend/src/contexts/SocketProvider.js
@@ -2,7 +2,6 @@ import React from "react";
 import io from "socket.io-client";
 
 export const socket = io(
-  // `${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}/${process.env.REACT_APP_SERVER_NAMESPACE}`
   `${process.env.REACT_APP_SERVER_HOST}:${process.env.REACT_APP_SERVER_PORT}`
 );
 export const SocketContext = React.createContext();
