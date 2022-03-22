244081cfd32413e63a042dfbea486a4378641a37
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-03-22 10:00:11 +0300

    upgrade app gw

--- note-board-backend/src/app.gateway.ts
+++ note-board-backend/src/app.gateway.ts
@@ -12,64 +12,58 @@ import {
 import { Server } from 'http';
 import { Socket } from 'socket.io';
 import { NotesService } from './notes/notes.service';
-import { UsersService } from './users/users.service';
 
-// @WebSocketGateway(Number(process.env.GW_PORT), { непонятно, как законектитться по другому порту и с другим неймспейсом
-//   namespace: process.env.GW_NAMESPACE,
-//   cors: true,
-// })
 @WebSocketGateway({
   cors: true,
 })
 export class AppGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly notesService: NotesService,
-  ) {}
+  constructor(private readonly notesService: NotesService) {}
 
   @WebSocketServer()
   private wss: Server;
 
   private logger: Logger = new Logger('AppGateway');
-  private count = 0;
+  private countClients = 0;
 
   afterInit(server: any) {
     this.logger.log('AppGateway Initialized', server);
   }
 
   async handleConnection(client: any) {
-    this.count = this.count + 1;
-    this.logger.log(`Connected: ${this.count}, Connection id: ${client.id}`);
+    this.countClients = this.countClients + 1;
+    this.logger.log(
+      `Connected: ${this.countClients}, Connection id: ${client.id}`,
+    );
     const notes = await this.notesService.getAll();
     client.emit('all-notes-to-client', notes);
   }
 
   handleDisconnect(client: any) {
-    this.count = this.count - 1;
+    this.countClients = this.countClients - 1;
     this.logger.log(
-      `Disconnected: ${this.count}, Disonnection id: ${client.id}`,
+      `Disconnected: ${this.countClients}, Disonnection id: ${client.id}`,
     );
   }
 
   @SubscribeMessage('create-note-to-server')
-  async createNote(@MessageBody() data) {
-    this.logger.log(`Create note`);
+  async createNote(@MessageBody() data, @ConnectedSocket() client: Socket) {
+    this.logger.log(`Create note from id: ${client.id}`);
     const note = await this.notesService.createNote(data);
     this.wss.emit('create-note-to-client', note);
   }
 
   @SubscribeMessage('update-note-to-server')
   async updateNote(@MessageBody() data, @ConnectedSocket() client: Socket) {
-    this.logger.log(`Update note`);
+    this.logger.log(`Update note from id: ${client.id}`);
     const note = await this.notesService.updateNote(data);
     client.broadcast.emit('update-note-to-client', note);
   }
 
   @SubscribeMessage('delete-note-to-server')
-  async deleteNote(@MessageBody() data) {
-    this.logger.log(`Delete note`);
+  async deleteNote(@MessageBody() data, @ConnectedSocket() client: Socket) {
+    this.logger.log(`Delete note from id: ${client.id}`);
     const note = await this.notesService.deleteNote(data);
     this.wss.emit('delete-note-to-client', note);
   }
